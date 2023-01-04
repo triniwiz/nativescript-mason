5a2ce5a1e91015c9e2469e020e8c6bfa9fbb7d4f
Osei Fortune <fortune.osei@yahoo.com>
2023-01-04 06:52:28 -0400

    Update TrackSizingFunction.swift

--- packages/nativescript-masonkit/src-native/mason-ios/Mason/Mason/TrackSizingFunction.swift
+++ packages/nativescript-masonkit/src-native/mason-ios/Mason/Mason/TrackSizingFunction.swift
@@ -7,7 +7,68 @@
 
 import Foundation
 
-public enum TrackSizingFunction {
-    case Single(MinMax)
-    case AutoRepeat(GridTrackRepetition, Array<MinMax>)
+
+
+@objc(TrackSizingFunction)
+@objcMembers
+public class TrackSizingFunction: NSObject {
+    var isRepeating: Bool
+    var singleValue: MinMax?
+    var gridTrackRepetition: GridTrackRepetition?
+    var repeatValue: Array<MinMax>?
+    
+    var cValue: UnsafeMutablePointer<CMasonTrackSizingFunction>? = nil
+    
+    init(isRepeating: Bool, singleValue: MinMax? = nil, gridTrackRepetition: GridTrackRepetition? = nil, repeatValue: Array<MinMax>? = nil) {
+        self.isRepeating = isRepeating
+        self.singleValue = singleValue
+        self.gridTrackRepetition = gridTrackRepetition
+        self.repeatValue = repeatValue
+    }
+    
+    
+    public static func Single(_ value: MinMax) -> TrackSizingFunction {
+        return TrackSizingFunction(isRepeating: false, singleValue: value)
+    }
+    
+    var minMaxBuffer: UnsafeMutablePointer<CMasonMinMaxArray>? = nil
+    var minMaxBufferValues: UnsafeMutableBufferPointer<CMasonMinMax>? = nil
+    
+    public static func AutoRepeat(_ gridTrackRepetition: GridTrackRepetition, _ value: Array<MinMax>) -> TrackSizingFunction {
+        let ret = TrackSizingFunction(isRepeating: true, singleValue: nil, gridTrackRepetition: gridTrackRepetition, repeatValue: value)
+        
+      
+        let minMax = UnsafeMutablePointer<CMasonMinMaxArray>.allocate(capacity: MemoryLayout<CMasonMinMaxArray>.size)
+        
+        let minMaxValues = UnsafeMutableBufferPointer<CMasonMinMax>.allocate(capacity: value.count)
+        
+        let _ = minMaxValues.initialize(from: value.map({ value in
+            value.cValue
+        }))
+        
+        
+        ret.minMaxBufferValues = minMaxValues
+        
+        let minMaxArray = CMasonMinMaxArray(array: minMaxValues.baseAddress, length: UInt(value.count))
+        
+        minMax.initialize(to: minMaxArray)
+        
+        ret.minMaxBuffer = minMax
+        
+        let tracking = UnsafeMutablePointer<CMasonTrackSizingFunction>.allocate(capacity: MemoryLayout<CMasonTrackSizingFunction>.size)
+        
+        var trackingValue = CMasonTrackSizingFunction()
+        
+        trackingValue.tag = Repeat
+        
+        trackingValue.repeat = Repeat_Body(_0: gridTrackRepetition.rawValue, _1: minMax)
+        
+        tracking.initialize(to: trackingValue)
+        
+        ret.cValue = tracking
+        
+       return ret
+    }
+    
+    
 }
