b780b946b5145f6b49223e369bee8769b6ba0f45
Osei Fortune <fortune.osei@yahoo.com>
2023-01-04 17:57:01 -0400

    fix: ios uiview types

--- packages/nativescript-masonkit/typings/ios.d.ts
+++ packages/nativescript-masonkit/typings/ios.d.ts
@@ -10,13 +10,19 @@ interface UIView {
 
   addSubviewsAt(views: NSArray<UIView> | UIView[], index: number): void;
 
-  setFlexGap(width: number, height: number): void;
+  setGap(width: number, height: number): void;
+
+  setInset(left: number, top: number, right: number, bottom: number): void;
+
+  setMargin(left: number, top: number, right: number, bottom: number): void;
+
   setMaxSize(width: number, height: number): void;
+
   setMinSize(width: number, height: number): void;
+
   setSize(width: number, height: number): void;
 
-  setPosition(left: number, top: number, right: number, bottom: number): void;
-  setMargin(left: number, top: number, right: number, bottom: number): void;
   setPadding(left: number, top: number, right: number, bottom: number): void;
+
   setBorder(left: number, top: number, right: number, bottom: number): void;
 }
