2ad178e1a8f0ada4990ca835c20ee7158682d552
Osei Fortune <fortune.osei@yahoo.com>
2023-01-06 06:24:13 -0400

    chore: grid demo

--- apps/demo/src/app.ts
+++ apps/demo/src/app.ts
@@ -5,7 +5,7 @@ declare const __non_webpack_require__;
 // load with java system before requiring which in turn calls dlopen
 //java.lang.System.loadLibrary('masonnative');
 
-__non_webpack_require__('system_lib://libmasonnativev8.so');
+//__non_webpack_require__('system_lib://libmasonnativev8.so');
 
 //__non_webpack_require__('./libmasonnativev8.so');
 
