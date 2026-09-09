2882628441bd1eddde5f0b13e7d621e1b657c992
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2026-09-09 15:17:07 +0800

    Remove DeepWebLayoutBenchmark

--- tools/benchmarks/python-linker-shim.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal EnableExtensions DisableDelayedExpansion
-for %%I in ("%RUST_ANDROID_GRADLE_PYTHON_COMMAND%") do set "MASON_LINKER_SHIM_DIR=%%~dpI"
-set "MASON_LINKER_RAW_ARGUMENTS=%*"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%MASON_LINKER_SHIM_DIR%python-linker-shim.ps1"
-exit /b %ERRORLEVEL%
